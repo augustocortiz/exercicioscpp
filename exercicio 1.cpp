2f4ed20dc054259ca12ee040752c88bc4cd0e727
augustocortiz <107955410+augustocortiz@users.noreply.github.com>
2022-06-22 12:31:09 -0300

    Update exercicio 1.cpp

--- exercicio 1.cpp
+++ exercicio 1.cpp
@@ -1,7 +1,7 @@
 /*
-1) Fazer um programa em C que calcule o valor da função f(x), 
+1) Fazer um programa em C que calcule o valor da funÃ§Ã£o f(x), 
 para x variando de 0 a 10 (x=0,1,2,3,....10)
-F(x) =2x² –3x  + 5
+F(x) =2xÂ² â€“3x  + 5
 */
 #include<iostream>
 #include<locale.h>
@@ -13,11 +13,11 @@ int main(){
 	setlocale(LC_ALL,"");
 	x = 1;
 
-	cout << "f(x) = 2x²-3x+5\n";
+	cout << "f(x) = 2xÂ²-3x+5\n";
 
 	while ( x<=10){
 		
-		cout << "\nf("<<x<<") = 2("<<x<<")²-3("<<x<<")+5 = " << (2*x*x)-(3*x)+5 << endl;
+		cout << "\nf("<<x<<") = 2("<<x<<")Â²-3("<<x<<")+5 = " << 2*(x*x)-(3*x)+5 << endl;
 		
 		x++;
 	}	
